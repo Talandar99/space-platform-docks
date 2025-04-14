7757e448d88ac48ad57b11e26e522cc375e6dffb
gamma-delta <petrathekat@gmail.com>
2025-04-14 15:52:46 -0500

    ui is even better!

--- control/ui.lua
+++ control/ui.lua
@@ -2,8 +2,68 @@
 
 local ui_lib = {events={}}
 
-local function make_dock_view(ui, dock)
-  local frame = ui.add{
+local STATUS_LOOKUP = {}
+for stat, number in pairs(defines.entity_status) do
+  STATUS_LOOKUP[number] = stat:gsub("_", "-")
+end
+local DIODE2SPRITE = {
+  [defines.entity_status_diode.green] = "status_working",
+  [defines.entity_status_diode.yellow] = "status_yellow",
+  [defines.entity_status_diode.red] = "status_not_working",
+}
+
+local function make_status_with_diode(parent, dock)
+  local ui = parent.add{type="flow"}
+  ui.style.padding = 8
+
+  -- https://forums.factorio.com/viewtopic.php?p=538298
+  local diode_color, status_name
+  if dock.custom_status then
+    status_name = dock.custom_status.label
+    diode_color = dock.custom_status.diode
+  else
+    status_name = {"entity-status." ..  STATUS_LOOKUP[dock.status] }
+    -- AUGH todo
+    diode_color = defines.entity_status_diode.green
+  end
+
+  ui.add{
+    type="sprite",
+    -- https://lua-api.factorio.com/latest/concepts/SpritePath.html
+    sprite="utility/" .. DIODE2SPRITE[diode_color],
+  }
+  ui.add{
+    type="label",
+    caption=status_name
+  }
+end
+
+local function make_dock_status(parent, dock)
+  local left_info = parent.add{
+    type="frame", direction="vertical",
+    style="inside_shallow_frame"
+  }
+  left_info.style.vertically_stretchable = true
+
+  make_status_with_diode(left_info, dock)
+
+  local preview_frame_frame = left_info.add{type="flow"}
+  preview_frame_frame.style.left_padding = 16
+  preview_frame_frame.style.right_padding = 16
+  local preview_frame = preview_frame_frame.add{type="frame", style="entity_frame"}
+  preview_frame.style.padding = 0
+  local preview = preview_frame.add{type="entity-preview"}
+  -- false for some reason??
+  preview.visible = true
+  -- preview.style.horizontally_stretchable = true
+  -- preview.style.vertically_stretchable = true
+  preview.style.minimal_width = 384
+  preview.style.minimal_height = 192
+  preview.entity = dock
+end
+
+local function make_dock_view(parent, dock)
+  local frame = parent.add{
     type="frame", direction="vertical",
     tags = {pkspd_dock_picker=true, viewing=dock.unit_number}
   }
@@ -20,8 +80,9 @@ local function make_dock_view(ui, dock)
 
   local label = frame.add{
     type="label",
-    caption={"pkspd-gui.on-platform", dock.surface.localised_name}
+    caption={"pkspd-gui.on-platform", dock.surface.platform.name}
   }
+  label.style.horizontal_align = "center"
   
   return frame
 end
@@ -29,15 +90,11 @@ end
 local function make_manual_tab(evt, tab_holder)
   local manual_tab = tab_holder.add{type="tab", caption={"pkspd-gui.manual-mode"}}
 
-  local manual_contents = tab_holder.add{type="flow", direction="vertical"}
-  manual_contents.add{
-    type="label",
-    caption={"pkspd-gui.pick-a-dock"},
-    style="frame_title"
+  local manual_contents = tab_holder.add{
+    type="flow", direction="vertical"
   }
 
   local dock_view_holder = manual_contents
-    .add{type="frame"}
     .add{
       type="scroll-pane",
       horizontal_scroll_policy="never",
@@ -52,7 +109,7 @@ local function make_manual_tab(evt, tab_holder)
   manual_contents.add{
     type="button",
     name="pkspd_dock_initiate_dock",
-    label={"pkspd-gui.initiate-dock"}
+    caption={"pkspd-gui.initiate-dock"}
   }
 
   tab_holder.add_tab(manual_tab, manual_contents)
@@ -118,13 +175,15 @@ ui_lib.events[defines.events.on_gui_opened] = function(evt)
     type="table", column_count = 2
   }
 
-  local left_info = main_content.add{type="frame"}
-  left_info.add{type="label", caption="Docked to a platform or something"}
+  make_dock_status(main_content, dock)
 
-  local tab_holder = main_content.add{
-    type = "tabbed-pane",
-    name = "pkspd_dock_tabbed_pane"
+  -- TODO: check out style frame_tabbed_pane
+  local right_controls = main_content.add{
+    type="frame",
+    style="inside_shallow_frame",
+    -- caption={"pkspd-gui.dock-controls"}
   }
+  local tab_holder = right_controls.add{type="tabbed-pane"}
   make_manual_tab(evt, tab_holder)
   make_auto_tab(evt, tab_holder)
 end
